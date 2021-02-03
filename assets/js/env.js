6a681790683f319583c5b6bd5823aad5fc26003f
nakamura196 <na.kamura.1263@gmail.com>
2021-02-04 07:14:48 +0900

    add

--- assets/js/env.js
+++ assets/js/env.js
@@ -5,6 +5,6 @@ const process = {
       : 'https://www.hi.u-tokyo.ac.jp/dev',
     DATA_URL: location.href.includes('localhost')
       ? 'http://localhost:5500'
-      : 'https://raw.githubusercontent.com/hi-ut/static_test/master',
+      : 'https://hi-ut.github.io/static_test',
   },
 }
